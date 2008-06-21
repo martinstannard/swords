e78926ac1f4b6077c2b528f54dea5a923ab01f40
Martin Stannard <mstannard@gmail.com>
2008-06-21 20:38:28 +1000

    refactored and added Grid class

--- crossword.rb
+++ crossword.rb
@@ -2,6 +2,7 @@
 require 'lib/parser'
 require 'pathname'
 require 'pp'
+require 'yaml'
 
 Patterns = [
               'xxxxxxx xxxxxxx',
@@ -27,48 +28,50 @@ HP = {[0,0] =>  6, [2,3]  =>  4, [4,0] =>  4, [6,1] => 6 }
 VP = {[0,1] =>  7, [0,5]  =>  7}
 
 
-GRID = [
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil],
-  [nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil]
-]
+class Grid
 
+  def initialize(columns = 15, rows = 15)
+    @columns = columns
+    @rows = rows
+    @grid = Hash.new {|h, k| h[k] = nil }
+  end
 
+  def put(x, y, value)
+    coord = [x,y]
+    @grid[coord] = value
+  end
+
+  def get(x, y)
+    coord = [x,y]
+    @grid[coord]
+  end
+
+  def display
+    @rows.times do |y|
+      @columns.times do |x|
+        coord = [x,y]
+        cell = @grid[coord]
+        print(cell.nil? ? " " : cell)
+      end
+      puts
+    end
+  end
+
+end
 
 class Crossworder
 
-  
   def initialize(options = {})
     @used_words = []
     @used_words = []
+    @x_size = options[:x_size] || 15
+    @y_size = options[:x_size] || 15
     @dict_words = File.readlines("/usr/share/dict/words").sort_by { rand }
-    @v_pattern = options[:v_pattern] || {}
     @requested_words = options[:requested_words] || []
-    @grid = options[:grid] || GRID
     @parser = Parser.new(Patterns)
     @h_pattern = @parser.horizontal_words
-  end
-
-  def read_q_and_a
-    @q_and_a = YAML.load(open('lib/q_and_a.yml'))
+    @v_pattern = options[:v_pattern] || {}
+    @grid = Grid.new
   end
 
   def build
@@ -77,25 +80,28 @@ class Crossworder
     incomplete = true
     while incomplete == true do
       @h_pattern.each do |h_pat|
+        #h_pat = @h_pattern[0]
         coord = [h_pat.x_pos, h_pat.y_pos]
-        puts coord
         length = h_pat.length
-        word = find_word(find_horiz_pattern(coord, length).join, coord, length)
-        puts word
+        word = find_word(find_horiz_pattern(coord, length), coord, length)
         incomplete = false if word 
         h_words << [word, coord]
       end
 
       h_words.each { |word| stuff_into_words_horiz(*word) if word[0]}
 
- #     @v_pattern.each_pair do |coord, length|
- #       word = find_word(find_vert_pattern(coord, length), coord, length)
- #       incomplete = false if word 
- #       v_words << [word, coord]
- #     end
+      #     @v_pattern.each_pair do |coord, length|
+      #       word = find_word(find_vert_pattern(coord, length), coord, length)
+      #       incomplete = false if word 
+      #       v_words << [word, coord]
+      #     end
     end
 
-#    v_words.each { |word| stuff_into_words_vert(*word) if word[0] }
+    #    v_words.each { |word| stuff_into_words_vert(*word) if word[0] }
+  end
+
+  def display
+    @grid.display
   end
 
   def find_word(pattern, coord, length)
@@ -117,39 +123,33 @@ class Crossworder
     nil
   end
 
-  def display
-    @grid.each do |line|
-      line.each do |c|
-        print(c.nil? ? "*" : c)
-      end
-      puts
-    end
-  end
-
   def find_horiz_pattern(coord, length)
-    row = @grid[coord[0]]
-    pattern = row[coord[1]..length-1].collect {|cell| cell.nil? ? '\w' : cell}
+    pattern = ''
+    0.upto(length-1) do |i|
+      cell = @grid.get(coord[0] + i, coord[1])
+      pattern += (cell.nil? ? '\w' : cell)
+    end
     pattern
   end
 
   def find_vert_pattern(coord, length)
-    re = ''
-    0.upto(length-1) do |offset|
-      cell = @grid[coord[0] + offset][coord[1]]
-      re += cell.nil? ? '\w' : cell
+    pattern = ''
+    0.upto(length-1) do |i|
+      cell = @grid.get(coord[0], coord[1] + i)
+      pattern += (cell.nil? ? '\w' : cell)
     end
-    re
+    pattern
   end
 
-  def stuff_into_words_horiz(line, coords)
+  def stuff_into_words_horiz(line, coord)
     line.split(//).each_with_index do |char, i|
-      @grid[coords[0]][i + coords[1]] = char
+      @grid.put(coord[0] + i, coord[1], char)
     end
   end
 
-  def stuff_into_words_vert(line, coords)
+  def stuff_into_words_vert(line, coord)
     line.split(//).each_with_index do |char, i|
-      @grid[i][coords[1]] = char
+      @grid.put(coord[0], coord[1] + 1, char)
     end
   end
 
@@ -158,7 +158,7 @@ end
 
 if __FILE__ == $0
 
-  cw = Crossworder.new({:h_pattern => HP, :v_pattern => VP})#, :grid => GRID})
+  cw = Crossworder.new({:h_pattern => HP, :v_pattern => VP})
 
   cw.build
 
