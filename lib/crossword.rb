9fa61e63b96d7dcd5be1b1c09e4ca44140cdb891
Dylan Egan <degan@engineyard.com>
2008-06-22 11:02:19 +1000

    Rewrite some further. Dictionary class for handling the dictionary.

--- lib/crossword.rb
+++ lib/crossword.rb
@@ -2,54 +2,48 @@
 require 'pathname'
 require "yaml"
 require 'pp'
+require 'crossword/dictionary'
 require 'crossword/grid'
 require 'crossword/parser'
 
-Patterns = [
-              'xxxxxxx xxxxxxx',
-              ' x x x x x x x ',
-              'xxxxxxxxxx xxxx',
-              ' x x x x x x x ',
-              'xxxxxxxxxxxxxxx',
-              ' x   x x x x x ',
-              'xxxxxxxxx xxxxx',
-              '   x x x x x   ',
-              'xxxxx xxxxxxxxx',
-              ' x x x x x   x ',
-              'xxxxxxxxxxxxxxx',
-              ' x x x x x x x ',
-              'xxxx xxxxxxxxxx',
-              ' x x x x x x x ',
-              'xxxxxxx xxxxxxx'
-            ]
-
 class Crossworder
-  def initialize(options = {})
-    @used_words = {
-                    :horizontal  =>  [],
-                    :vertical  =>  []
-                  }
-    @dict = YAML.load_file(File.dirname(__FILE__) + '/../yaml/dictionary.yml')
-    @dict_words = @dict.keys.sort_by {rand}
+  attr_accessor :dictionary, :grid, :requested_words
+
+  def initialize    
+    pattern = [   'xxxxxxx xxxxxxx',
+                  ' x x x x x x x ',
+                  'xxxxxxxxxx xxxx',
+                  ' x x x x x x x ',
+                  'xxxxxxxxxxxxxxx',
+                  ' x   x x x x x ',
+                  'xxxxxxxxx xxxxx',
+                  '   x x x x x   ',
+                  'xxxxx xxxxxxxxx',
+                  ' x x x x x   x ',
+                  'xxxxxxxxxxxxxxx',
+                  ' x x x x x x x ',
+                  'xxxx xxxxxxxxxx',
+                  ' x x x x x x x ',
+                  'xxxxxxx xxxxxxx'  ]
+                
+    @dictionary = Crossword::Dictionary.new
+    @grid = Crossword::Grid.new
+    @parser = Crossword::Parser.new(pattern)
+
     @requested_words = [] #options[:requested_words] || ['rails','ruby','beer','jour']
-    @parser = Parser.new(Patterns)
     @h_pattern = @parser.horizontal_words
     @v_pattern = @parser.vertical_words
-    @grid = Grid.new
     @h_words = []
     @v_words = []
-
   end
 
   def build
-  
     @h_pattern.each do |word_vector|
-      
       dir = :horizontal
       len = word_vector.length
       coord = [word_vector.x_pos,word_vector.y_pos]
       pattern = find_horiz_pattern(coord,len)
-      word = find_word(pattern, coord, len, dir)
+      word = @dictionary.find_word(pattern, coord, len, dir, @requested_words)
       @h_words << [word, coord]
       @h_words.each { |word| stuff_into_words_horiz(*word) if word[0]}
     end
@@ -62,50 +56,28 @@ class Crossworder
         len = word_vector.length
         coord = [word_vector.x_pos,word_vector.y_pos]
         pattern = find_vert_pattern(coord,len)
-        word = find_word(pattern, coord, len, dir)
+        word = @dictionary.find_word(pattern, coord, len, dir, @requested_words)
         all_is_well = false if word.nil?
         @v_words << [word, coord]
         @v_words.each { |word| stuff_into_words_vert(*word) if word[0]}
       end
-      
     end
     #The vertical words aren't being filled in completely. It's probably hard to find words fitting the requirements. If we do the loop we need to make it fill it could end up taking a very long time, I suppose.
     #Either we don't care, or we rethink this... If we didn't care we can put it into the rails app and the UI can be sorted and the caring can happen later!
-    
-  end
-
-  def find_word(pattern, coord, length, dir)
-    word = find_word_from_dict(pattern, coord, length, @requested_words, dir)
-    find_word_from_dict(pattern, coord, length, @dict_words, dir) unless word
-  end
-
-  def find_word_from_dict(pattern, coord, length, word_list, dir)
-    word_list.each do |l|
-      line = l.to_s.strip
-      next unless line.size == length
-      r = Regexp.new(pattern)
-      md = line.match(r)
-      if !md.nil? && !@used_words[dir].include?(line) && !line.match(/[A-Z]/)
-        @used_words[dir] << line
-        return line
-      end
-    end 
-    nil
   end
 
   def display
     @grid.display
     puts "Across:"
-    display_clues @used_words[:horizontal]
+    display_clues @dictionary.used_words[:horizontal]
     puts "Down:"
-    display_clues @used_words[:vertical]
+    display_clues @dictionary.used_words[:vertical]
   end
 
   def display_clues(words)
     i = 1
-    puts words.inspect
     words.each do |word|
-      clues = @dict[word]["si"].split ", "
+      clues = @dictionary.dict[word]["si"].split ", "
       clue = clues[rand*10 % clues.length]
       puts "#{i}. " + clue
       i = i + 1
--- lib/crossword/grid.rb
+++ lib/crossword/grid.rb
@@ -1,28 +1,30 @@
-class Grid
-  def initialize(columns = 15, rows = 15)
-    @columns = columns
-    @rows = rows
-    @grid = Hash.new {|h, k| h[k] = nil }
-  end
+module Crossword
+  class Grid
+    def initialize(columns = 15, rows = 15)
+      @columns = columns
+      @rows = rows
+      @grid = Hash.new {|h, k| h[k] = nil }
+    end
 
-  def put(x, y, value)
-    coord = [x,y]
-    @grid[coord] = value
-  end
+    def put(x, y, value)
+      coord = [x,y]
+      @grid[coord] = value
+    end
 
-  def get(x, y)
-    coord = [x,y]
-    @grid[coord]
-  end
+    def get(x, y)
+      coord = [x,y]
+      @grid[coord]
+    end
 
-  def display
-    @rows.times do |y|
-      @columns.times do |x|
-        coord = [x,y]
-        cell = @grid[coord]
-        print(cell.nil? ? " " : cell)
+    def display
+      @rows.times do |y|
+        @columns.times do |x|
+          coord = [x,y]
+          cell = @grid[coord]
+          print(cell.nil? ? " " : cell)
+        end
+        puts
       end
-      puts
     end
   end
 end
